fa6c74682166d4b71299ce7de4e60cfaf5ac07e6
maria aristizabal <maria.aristizabal@dlogin04.dogwood.wcoss2.ncep.noaa.gov>
2023-04-10 14:53:55 +0000

    Added longitud conversion to constrain the longitude limits from RTOFS between -180 and 180. This is to avoid a conflict with the Cartopy projection PlateCarree

--- ush/python/ocean/plot_ohc.py
+++ ush/python/ocean/plot_ohc.py
@@ -78,6 +78,15 @@ var0 = ncfile0['ocean_heat_content']
 lon = np.asarray(ncfile0.Longitude)
 lat = np.asarray(ncfile0.Latitude)
 
+lonmin_raw = np.min(lon)
+lonmax_raw = np.max(lon)
+print('raw lonlat limit: ', np.min(lon), np.max(lon), np.min(lat), np.max(lat))
+
+# Constrain lon limits between -180 and 180 so it does not conflict with the cartopy projection PlateCarree
+lon[lon>180] = lon[lon>180] - 360
+sort_lon = np.argsort(lon)
+lon = lon[sort_lon]
+
 # reduce array size to 2D
 temp = np.squeeze(temp)
 var0 = np.squeeze(var0)
@@ -94,14 +103,17 @@ lonmin = np.min(lon)
 lonmax = np.max(lon)
 latmin = np.min(lat)
 latmax = np.max(lat)
+print('new lonlat limit: ', np.min(lon), np.max(lon), np.min(lat), np.max(lat))
+
 var_name = 'ohc'
 units = '($kJ/cm^2$)'
 
 # Shift central longitude so the Southern Hemisphere and North Indin Ocean domains are plotted continuously
-if lonmin > 180:
-    central_longitude = 0
+if np.logical_and(lonmax >= 90, lonmax <=180):
+    central_longitude = 90
 else:
-    central_longitude = 200
+    central_longitude = -90
+print('central longitude: ',central_longitude)
 
 count = len(afiles)        
 for k in range(count):
@@ -114,6 +126,8 @@ for k in range(count):
    var = np.reshape(np.asarray(var),(ind[0]*ind[1],1))
    var[np.argwhere(np.isnan(temp))] = np.nan
    var = np.reshape(var,(ind[0],ind[1]))
+   # sort var according to the new longitude
+   var = var[:,sort_lon]
 
    # define forecast hour
    fhr=k*6
@@ -139,7 +153,7 @@ for k in range(count):
          ax.plot(aln,alt,'-ok',markersize=2,alpha=0.4,transform=ccrs.PlateCarree(central_longitude=0))
          if k < len(aln):
             ax.plot(aln[k],alt[k],'ok',markersize=6,alpha=0.4,markerfacecolor='None',transform=ccrs.PlateCarree(central_longitude=0))
-   ax.set_extent([lonmin, lonmax, latmin, latmax], crs=ccrs.PlateCarree())
+   ax.set_extent([lonmin_raw, lonmax_raw, latmin, latmax], crs=ccrs.PlateCarree())
 
    # Add gridlines and labels   
 #  gl = ax.gridlines(crs=transform, draw_labels=True, linewidth=0.3, color='0.1', alpha=0.6, linestyle=(0, (5, 10)))
