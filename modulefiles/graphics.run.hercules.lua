86d80e032e94c564941f2555ad01d768ebd2bbe9
Maria.Aristizabal <Maria.Aristizabal@noaa.gov>
2024-02-29 19:13:22 +0000

    Update the version of spack-stack in graphics.run.hercules.lua and load imagemagick

--- modulefiles/graphics.run.hercules.lua
+++ modulefiles/graphics.run.hercules.lua
@@ -1,11 +1,11 @@
 help([[
-loads HAFS application level modulefile on Orion
+loads HAFS application level modulefile on Hercules
 ]])
 
 ncl_ver=os.getenv("ncl") or "6.6.2"
 load(pathJoin("ncl", ncl_ver))
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0" 
 load(pathJoin("stack-intel", stack_intel_ver))
@@ -19,11 +19,13 @@ load(pathJoin("hdf5", hdf5_ver))
 netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
 load(pathJoin("netcdf-c", netcdf_c_ver))
 
-netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
+netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.1"
 load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
 
 wgrib2_ver=os.getenv("wgrib2_ver") or "3.1.1"
 load(pathJoin("wgrib2", wgrib2_ver))
+
+load("imagemagick")
 
 prepend_path("MODULEPATH", "/work/noaa/hwrf/noscrub/local/modulefiles")
 load(pathJoin("python", "wcoss2_env"))
