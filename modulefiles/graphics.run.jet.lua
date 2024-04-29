39f9a9d120d033c1912ef40d5defebfb4a06d923
Biju <biju.thomas@noaa.gov>
2024-04-29 19:19:16 +0000

    updating modulefiles on Jet

--- modulefiles/graphics.run.jet.lua
+++ modulefiles/graphics.run.jet.lua
@@ -2,12 +2,14 @@ help([[
 loads HAFS application level modulefile on Jet
 ]])
 
-prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
+--prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 load(pathJoin("stack-intel", stack_intel_ver))
 
-ncl_ver=os.getenv("ncl") or "6.5.0"
+--ncl_ver=os.getenv("ncl") or "6.5.0"
+ncl_ver=os.getenv("ncl") or "6.6.2"
 load(pathJoin("ncl", ncl_ver))
 
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
@@ -19,12 +21,16 @@ load(pathJoin("hdf5", hdf5_ver))
 netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
 load(pathJoin("netcdf-c", netcdf_c_ver))
 
-netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
+--netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
+netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.1"
 load(pathJoin("netcdf-fortran", netcdf_fortran_ver)) 
 
 wgrib2_ver=os.getenv("wgrib2_ver") or "2.0.8"
 load(pathJoin("wgrib2", wgrib2_ver))
 setenv("WGRIB2", "/mnt/lfs4/HFIP/hwrfv3/Jili.Dong/hafs_tools/grib2/wgrib2/wgrib2")
+
+imagemagick_ver=os.getenv("imagemagick_ver") or "7.1.1-11"
+load(pathJoin("imagemagick", imagemagick_ver))
 
 prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hwrfv3/local/modulefiles")
 load(pathJoin("python", "wcoss2_env"))
